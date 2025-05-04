662c6d916ad0c980a692eeed91912a119d91d0a5
Jung-kr <jungwy98@gmail.com>
2025-05-04 20:55:23 +0900

    initial commit

---
+++ money-example/src/tdd/MoneyTest.java
@@ -0,0 +1,11 @@
+package tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @Test
+    void testMultiplication() {
+        Assertions.assertEquals(1, 1);
+    }
+}
